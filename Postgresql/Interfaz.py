e1fd77a146131d753991acecba62ee9f59e5a644
ed-quiroga-2103 <ed.quiroga.2103@gmail.com>
2019-10-13 19:48:05 -0600

    Add Restock functions

--- Postgresql/Interfaz.py
+++ Postgresql/Interfaz.py
@@ -439,3 +439,179 @@ def initStore(idStore):
 
       return 1
 
+def getItemsWarehouse():
+
+      connection = pg.DB(dbname='testpsql', host='127.0.0.1', port = 5432, user='root', passwd='root')
+
+      data = connection.query("SELECT IdItem FROM ItemWarehouse WHERE Quantity = 0;")
+
+      ids = []
+
+      connection.close()
+
+      for line in data:
+            ids.append(line[0])
+
+      return ids
+
+def generateWarehouseRequest():
+
+      connection = pg.DB(dbname='testpsql', host='127.0.0.1', port = 5432, user='root', passwd='root')
+
+      data = getItemsWarehouse()
+
+
+      idRequest = connection.query("SELECT IdRequest FROM WarehouseRequest ORDER BY IdRequest DESC;")
+
+      try:
+
+        idRequest = idRequest[0][0] + 1 
+
+      except:
+
+        idRequest = 1
+
+      today = date.today()
+      requestDate = today.strftime("%Y-%m-%d")
+
+
+      connection.query("INSERT INTO WarehouseRequest VALUES " + str( (idRequest, requestDate) ) + ";")
+
+
+      for i in data:
+
+            query = "INSERT INTO WarehouseRequestItem VALUES "
+
+            query += str( (idRequest, i, 1) ) + ";"
+
+            connection.query(query)
+
+      connection.close()
+
+      restockWarehouse(data)
+
+def restockWarehouse(idList):
+      connection = pg.DB(dbname='testpsql', host='127.0.0.1', port = 5432, user='root', passwd='root')
+
+
+
+      for i in idList:
+
+            query = "UPDATE ItemWarehouse SET Quantity = 25 WHERE IdItem = " + str(i) + ";"
+
+            connection.query(query)
+
+      connection.close()
+            
+def getStockFromWarehouse(idItem):
+
+    connection = pg.DB(host='localhost',
+                        user='root',
+                        passwd='root',
+                        dbname='testpsql')
+
+
+    data = connection.query("SELECT Quantity FROM ItemWarehouse WHERE IdItem = " + str(idItem) + ";")
+
+    connection.close()
+
+    return data[0][0]
+
+def getItemsForRestock():
+
+      query = "SELECT IdRequest, IdItem, Quantity FROM StoreRequestItem ORDER BY IdRequest;"
+
+      connection = pg.DB(host='localhost',
+                        user='root',
+                        passwd='root',
+                        dbname='testpsql')
+
+      return connection.query(query)
+
+def updateWarehouseStock(idItem, newStock):
+
+      connection = pg.DB(host='localhost',
+                        user='root',
+                        passwd='root',
+                        dbname='testpsql')
+
+      query = "UPDATE ItemWarehouse SET Quantity = " + str(newStock)
+      query += "WHERE IdItem = " + str(idItem) + ";" 
+
+      connection.query(query)
+
+      connection.close()
+
+def restockStores():
+
+      items = getItemsForRestock()
+
+      connection = pg.DB(host='localhost',
+                        user='root',
+                        passwd='root',
+                        dbname='testpsql')
+
+      for item in items:
+
+            idStore = getStoreFromRequest(item[0])
+            
+            stock = getStockFromWarehouse(item[1])
+
+            needed = item[2]
+
+            newValue = 0
+
+            if stock > needed:
+                  newValue = needed + getItemStock(item[1], idStore)
+                  stock -= needed
+
+                  updateWarehouseStock(item[1], stock)
+                  query = "UPDATE StoreRequestItem SET Status = 1 WHERE IdItem = " + str(item[1]) 
+                  query += " AND IdRequest = " + str(item[0]) + ";" 
+                  connection.query(query)
+
+            else:
+                  newValue = stock + getItemStock(item[1], idStore)
+                  updateWarehouseStock(item[1], 0)
+
+                  needed -= stock
+
+                  query = "UPDATE StoreRequestItem SET Quantity = " + str(needed)
+                  query += " WHERE IdItem = " + str(item[1]) + " AND IdRequest = " + str(item[0]) + ";"
+
+                  connection.query(query)
+
+            query = "UPDATE ItemStore SET Quantity = " + str(newValue)
+
+            query += " WHERE IdStore = " + str(idStore)
+            query += " AND IdItem = " + str(item[1]) + ";"
+            print(query)
+            connection.query(query)
+
+
+def getStoreFromRequest(idRequest):
+
+      query = "SELECT IdStore FROM StoreRequest WHERE IdRequest = " + str(idRequest) + ";"
+      
+      connection = pg.DB(host='localhost',
+                        user='root',
+                        passwd='root',
+                        dbname='testpsql')
+
+      data = connection.query(query)
+
+      return data[0][0]
+
+def getItemStock(idItem, idStore):
+
+      query = "SELECT Quantity FROM ItemStore WHERE IdStore = " + str(idStore) + " "
+      query += "AND IdItem = " + str(idItem) + ";"
+
+      connection = pg.DB(host='localhost',
+                        user='root',
+                        passwd='root',
+                        dbname='testpsql')
+
+      data = connection.query(query)
+
+      return data[0][0]
